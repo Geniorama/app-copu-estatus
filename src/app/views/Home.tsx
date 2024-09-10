41a0b08fbbc71bd38f9891e2e9e531afa4b623b8
Darwin Roa <darwinmichaelroamora@gmail.com>
2024-09-10 15:06:25 -0500

    feat(utilities ui): Agrega el componente global de Inputs

--- src/app/views/Home.tsx
+++ src/app/views/Home.tsx
@@ -1,12 +1,18 @@
 "use client";
 
-import Button from "../utilities/ui/button";
+import Button from "../utilities/ui/Button";
+import Input from "../utilities/ui/Input";
 
 export default function Home() {
 
   return (
     <main>
       <h1>WELCOME: You are authenticated</h1>
+      <Input
+        type="text"
+        id="text"
+        placeholder="Input Type text"
+      />
       <Button type="button">Prueba</Button>
     </main>
   );
