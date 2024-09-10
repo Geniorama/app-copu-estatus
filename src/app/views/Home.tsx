826a33e688648edfc8041907f8884b158d750128
Darwin Roa <darwinmichaelroamora@gmail.com>
2024-09-10 11:59:37 -0500

    feat(utilities ui): Agrega el componente global de Buttons

--- src/app/views/Home.tsx
+++ src/app/views/Home.tsx
@@ -1,10 +1,13 @@
 "use client";
 
+import Button from "../utilities/ui/button";
+
 export default function Home() {
 
   return (
     <main>
       <h1>WELCOME: You are authenticated</h1>
+      <Button type="button">Prueba</Button>
     </main>
   );
 }
