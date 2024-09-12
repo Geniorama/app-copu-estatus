bf6df0c66075b53e4c622c44e9d548c8ad3507b0
Darwin Roa <darwinmichaelroamora@gmail.com>
2024-09-12 14:06:20 -0500

    feat(utility select): Agrega estilos al componente global de Select

--- src/app/utilities/ui/Select.tsx
+++ src/app/utilities/ui/Select.tsx
@@ -3,7 +3,7 @@ interface Props extends React.SelectHTMLAttributes<HTMLSelectElement> { }
 export function Select({ children, ...props }: Props) {
   return (
     <select
-      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
+      className="block w-full rounded-md border-0 py-3 px-4 text-cp-dark shadow-sm ring-1 ring-inset ring-gray-300"
       {...props}
     >
       {children}
