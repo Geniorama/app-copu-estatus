b55285ed5a43a439e10579213c4531b3ea5717a3
angelpublicista <angelpublicista.1@gmail.com>
2024-09-13 10:52:10 -0500

    fix: input styles

--- src/app/views/Dashboard/Home.tsx
+++ src/app/views/Dashboard/Home.tsx
@@ -1,98 +1,55 @@
-"use client"
+"use client";
 
-import { useSelector } from "react-redux"
-import { useEffect } from "react"
-import { RootState } from "@/app/store"
-import CardAction from "@/app/components/CardAction/CardAction"
-import Input from "@/app/utilities/ui/Input"
+import { useSelector } from "react-redux";
+import { useEffect } from "react";
+import { RootState } from "@/app/store";
+import CardAction from "@/app/components/CardAction/CardAction";
+import Input from "@/app/utilities/ui/Input";
+import Table from "@/app/components/Table/Table";
 
 export default function DashboardHome() {
-  const {currentUser} = useSelector((state:RootState) => state.user)
+  const { currentUser } = useSelector((state: RootState) => state.user);
 
   useEffect(() => {
-    if(currentUser){
-        console.log('Current user dashboard:',currentUser)
+    if (currentUser) {
+      console.log("Current user dashboard:", currentUser);
     }
-  },[currentUser])
+  }, [currentUser]);
+
+  const tableData = {
+    heads: ["ID", "Company", "Contact", "Services", "Quantity"],
+    rows: [
+      [
+        "1",
+        "Sancho BBDO",
+        "Juan Pérez",
+        "Servicio BBDO 1, Servicio BBDO 2",
+        "1",
+      ],
+      ["2", "Company XYZ", "Jane Doe", "<a href='' class='underline text-cp-primary'>Servicio XYZ</a>", "3"],
+      // más filas
+    ],
+  };
 
   return (
     <div>
       <div className="grid grid-cols-4 gap-5">
-        <CardAction 
-          title="Crear usuario"
-          icon="user"
-        />
-        <CardAction 
-          title="Crear empresa"
-          icon="company"
-        />
-        <CardAction 
-          title="Crear servicio"
-          icon="service"
-        />
-        <CardAction 
-          title="Crear contenido"
-          icon="content"
-        />
+        <CardAction title="Crear usuario" icon="user" />
+        <CardAction title="Crear empresa" icon="company" />
+        <CardAction title="Crear servicio" icon="service" />
+        <CardAction title="Crear contenido" icon="content" />
       </div>
 
       <div className="mt-8">
         <div className="flex justify-between items-center">
           <h3 className="text-xl font-bold">Mis empresas asignadas</h3>
           <div>
-            <Input 
-              placeholder="Buscar..."
-              type="search"
-            />
+            <Input placeholder="Buscar..." type="search" />
           </div>
         </div>
 
-        <table className="w-full mt-4 text-slate-300">
-          <thead className="text-slate-200 bg-gray-950">
-            <tr className="">
-              <th className="p-4 uppercase text-sm text-left">Id</th>
-              <th className="p-4 uppercase text-sm text-left">Nombre empresa</th>
-              <th className="p-4 uppercase text-sm text-left">Contacto</th>
-              <th className="p-4 uppercase text-sm text-left">Servicios activos</th>
-              <th className="p-4 uppercase text-sm text-left">Última modificación</th>
-            </tr>
-          </thead>
-          <tbody>
-              <tr className="bg-gray-800 hover:outline outline-1 outline-cp-primary">
-                <td className="p-4 text-sm text-left">1</td>
-                <td className="p-4 text-sm text-left">Sancho BBDO</td>
-                <td className="p-4 text-sm text-left">Juan Pérez</td>
-                <td className="p-4 text-sm text-left">
-                  <a className="underline text-green-500 hover:text-cp-primary" href="">Servicio BBDO 1</a>, {" "}
-                  <a className="underline text-green-500 hover:text-cp-primary" href="">Servicio BBDO 2</a>
-                </td>
-                <td className="p-2 py-4 text-sm text-left">1</td>
-              </tr>
-
-              <tr className="bg-gray-900 hover:outline outline-1 outline-cp-primary">
-                <td className="p-4 text-sm text-left">1</td>
-                <td className="p-4 text-sm text-left">Sancho BBDO</td>
-                <td className="p-4 text-sm text-left">Juan Pérez</td>
-                <td className="p-4 text-sm text-left">
-                  <a className="underline text-green-500 hover:text-cp-primary" href="">Servicio BBDO 1</a>, {" "}
-                  <a className="underline text-green-500 hover:text-cp-primary" href="">Servicio BBDO 2</a>
-                </td>
-                <td className="p-2 py-4 text-sm text-left">1</td>
-              </tr>
-
-              <tr className="bg-gray-800 hover:outline outline-1 outline-cp-primary">
-                <td className="p-4 text-sm text-left">1</td>
-                <td className="p-4 text-sm text-left">Sancho BBDO</td>
-                <td className="p-4 text-sm text-left">Juan Pérez</td>
-                <td className="p-4 text-sm text-left">
-                  <a className="underline text-green-500 hover:text-cp-primary" href="">Servicio BBDO 1</a>, {" "}
-                  <a className="underline text-green-500 hover:text-cp-primary" href="">Servicio BBDO 2</a>
-                </td>
-                <td className="p-2 py-4 text-sm text-left">1</td>
-              </tr>
-          </tbody>
-        </table>
+        <Table data={tableData} />
       </div>
     </div>
-  )
+  );
 }
