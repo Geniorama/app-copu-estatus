7a2bd52c548f56dd73e33affb09bce14dfa81093
Darwin Roa <darwinmichaelroamora@gmail.com>
2024-09-13 12:31:27 -0500

    feat(utilities ui): Actualiza estilos de componente global Label

--- src/app/utilities/ui/Label.tsx
+++ src/app/utilities/ui/Label.tsx
@@ -3,7 +3,7 @@ interface Props extends React.LabelHTMLAttributes<HTMLLabelElement> { }
 export function Label({ children, ...props }: Props) {
   return (
     <label
-      className="block text-base font-medium leading-6 text-cp-dark mb-1"
+      className="block text-base font-medium leading-6 text-slate-200 mb-1"
       {...props}
     >
       {children}
