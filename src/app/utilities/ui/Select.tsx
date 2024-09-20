0d8a082fa45bcea1533b47fa83bd332c5b8b7310
Darwin Roa <darwinmichaelroamora@gmail.com>
2024-09-20 11:27:14 -0500

    feat(util ui): Agrega la propiedad mode a los selects options para indicar si es light o dark

--- src/app/utilities/ui/Select.tsx
+++ src/app/utilities/ui/Select.tsx
@@ -4,16 +4,25 @@ interface Props extends React.SelectHTMLAttributes<HTMLSelectElement> {
     name: string;
   }[];
   defaultOptionText?: string;
+  mode?: "cp-light" | "cp-dark";
 }
 
-export function Select({ options, defaultOptionText = 'Selecciona una opción', ...props }: Props) {
+export function Select({ options, defaultOptionText = 'Selecciona una opción', mode, ...props }: Props) {
+  const classMode =
+    mode === "cp-dark"
+      ? "border-gray-400 text-cp-dark placeholder:text-gray-400"
+      : "border-slate-300 text-cp-light placeholder:text-gray-400";
+  const classOptions =
+    mode === "cp-dark"
+      ? "text-cp-dark bg-cp-light"
+      : "text-slate-300 bg-cp-dark";
   return (
     <select
-      className="block bg-transparent w-full rounded-md outline-none border-2 border-slate-300 text-cp-light py-3 px-4 placeholder:text-gray-400 focus-visible:border-cp-primary"
+      className={`block bg-transparent w-full rounded-md outline-none border-2 focus-visible:border-cp-primary py-3 px-4 ${classMode}`}
       {...props}
     >v
-      <option className="text-slate-300 bg-cp-dark" disabled selected>{defaultOptionText}</option>
-      {options.map(option => <option key={option.value} className="text-slate-300 bg-cp-dark" value={option.value}>{option.name}</option>)}
+      <option className={`${classOptions}`} disabled selected>{defaultOptionText}</option>
+      {options.map(option => <option key={option.value} className={`${classOptions}`} value={option.value}>{option.name}</option>)}
 
     </select>
   )
