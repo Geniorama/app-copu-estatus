13a69383f84be079c3b39528a440bca7d5c1b526
angelpublicista <angelpublicista.1@gmail.com>
2025-06-20 16:21:38 -0500

    fix: services client dashboard table

--- src/app/views/DashboardClient/HomeClient.tsx
+++ src/app/views/DashboardClient/HomeClient.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import TitleSection from "@/app/utilities/ui/TitleSection";
 import { formatNumber } from "@/app/utilities/helpers/formatters";
@@ -46,8 +46,7 @@ export default function HomeClient() {
       publicationDate: "2021-01-01",
       type: "Social Media",
     },
-    
-  ])
+  ]);
   const [services] = useState<Service[]>([
     {
       id: "1",
@@ -58,7 +57,16 @@ export default function HomeClient() {
       startDate: "2021-01-01",
       endDate: "2021-01-01",
     },
-  ])
+    {
+      id: "2",
+      name: "Servicio 2",
+      description: "Descripción del servicio 2",
+      features: [],
+      status: false,
+      startDate: "2021-01-01",
+      endDate: "2021-01-01",
+    },
+  ]);
   const [companies] = useState<Company[]>([
     {
       id: "1",
@@ -83,15 +91,36 @@ export default function HomeClient() {
   ]);
 
   const dataServices: TableDataProps = {
-    heads: ["Nombre", "Fecha inicio", "Fecha fin", "Estado"],
-    rows: services.map((service) => [service.name, service.startDate, service.endDate, service.status ? "Activo" : "Inactivo"]),
+    heads: ["Nombre", "Fecha inicio", "Fecha fin", ""],
+    rows: services.map((service) => [
+      <>
+        <span
+          key={service.id}
+          className={`${
+            service.status ? "bg-cp-primary" : "bg-red-500"
+          } w-2 h-2 rounded-full inline-block mr-1`}
+        ></span>{" "}
+        <span>{service.name}</span>
+      </>,
+      service.startDate,
+      service.endDate,
+      <span
+        key={service.id}
+        className="text-cp-primary hover:text-cp-primary-dark transition cursor-pointer hover:underline"
+      >
+        Ver
+      </span>,
+    ]),
   };
 
   const dataContents: TableDataProps = {
     heads: ["Nombre", "Fecha de publicación", "Tipo"],
-    rows: contents.map((content: Content) => [content.headline, content.publicationDate, content.type]),
+    rows: contents.map((content: Content) => [
+      content.headline,
+      content.publicationDate,
+      content.type,
+    ]),
   };
-
 
   return (
     <div>
@@ -133,7 +162,12 @@ export default function HomeClient() {
           <div className="mt-5 overflow-x-scroll w-full custom-scroll">
             <Table data={dataContents} />
           </div>
-          <Link href={'/dashboard/contenidos'} className="text-slate-400 text-center flex justify-center items-center mt-2 hover:text-cp-primary hover:underline transition">Ver todos</Link>
+          <Link
+            href={"/dashboard/contenidos"}
+            className="text-slate-400 text-center flex justify-center items-center mt-2 hover:text-cp-primary hover:underline transition"
+          >
+            Ver todos
+          </Link>
         </div>
       </div>
 
@@ -141,20 +175,36 @@ export default function HomeClient() {
         <div className="w-full lg:w-1/3 bg-slate-900 rounded-lg p-4">
           <h2>Contacto</h2>
           <div className="flex items-center gap-4 mt-4">
-              <div className="w-32 h-32 bg-slate-800 rounded-full flex justify-center items-center">
-                <FontAwesomeIcon className="text-slate-400 size-10" icon={faUser} />
-              </div>
-              <div>
-                <h3>{userExecutive.name}</h3>
-                <p className="mb-4 text-sm text-slate-400">{userExecutive.position}</p>
-                <Link href={userExecutive.whatsappUrl} target="_blank" rel="noopener noreferrer">
-                  <Button fullWidthMobile mode="cp-light">Grupo de WhatsApp</Button>
-                </Link>
-                <Link href={userExecutive.whatsappUrl} target="_blank" rel="noopener noreferrer" className="text-slate-400 hover:text-cp-primary hover:underline transition mt-2 block">
-                  Mensaje directo: {userExecutive.phone}
-                </Link>
-              </div>
+            <div className="w-32 h-32 bg-slate-800 rounded-full flex justify-center items-center">
+              <FontAwesomeIcon
+                className="text-slate-400 size-10"
+                icon={faUser}
+              />
             </div>
+            <div>
+              <h3>{userExecutive.name}</h3>
+              <p className="mb-4 text-sm text-slate-400">
+                {userExecutive.position}
+              </p>
+              <Link
+                href={userExecutive.whatsappUrl}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <Button fullWidthMobile mode="cp-light">
+                  Grupo de WhatsApp
+                </Button>
+              </Link>
+              <Link
+                href={userExecutive.whatsappUrl}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-slate-400 hover:text-cp-primary hover:underline transition mt-2 block"
+              >
+                Mensaje directo: {userExecutive.phone}
+              </Link>
+            </div>
+          </div>
         </div>
 
         <div className="w-full lg:w-1/3 bg-slate-900 rounded-lg p-4">
@@ -177,7 +227,12 @@ export default function HomeClient() {
               <div className="mt-0 overflow-x-scroll w-full custom-scroll">
                 <Table data={dataServices} />
               </div>
-              <Link href={'/dashboard/servicios'} className="text-slate-400 text-center flex justify-center items-center mt-2 hover:text-cp-primary hover:underline transition">Ver todos</Link>
+              <Link
+                href={"/dashboard/servicios"}
+                className="text-slate-400 text-center flex justify-center items-center mt-2 hover:text-cp-primary hover:underline transition"
+              >
+                Ver todos
+              </Link>
             </div>
           ) : (
             <div className="flex justify-center items-center mt-5 h-[100px]">
@@ -187,5 +242,5 @@ export default function HomeClient() {
         </div>
       </div>
     </div>
-  )
+  );
 }
