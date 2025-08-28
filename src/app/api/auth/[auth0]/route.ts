1f977eead9196505f59a7cd5c4172792e3f5947f
angelpublicista <angelpublicista.1@gmail.com>
2025-08-28 18:15:37 -0500

    fix: auth api

--- src/app/api/auth/[auth0]/route.ts
+++ src/app/api/auth/[auth0]/route.ts
@@ -1,6 +1,5 @@
 // app/api/auth/[auth0]/route.js
 import { handleAuth } from '@auth0/nextjs-auth0';
-import auth0Config from '@/app/lib/auth0';
 
-export const GET = handleAuth(auth0Config);
-export const POST = handleAuth(auth0Config);
+export const GET = handleAuth();
+export const POST = handleAuth();
